168111983b0e2f1ecb537d45e6d2dc84868ab53d
andredochute <86085474+andredochute@users.noreply.github.com>
2022-09-12 22:50:07 -0300

    Fiat: O menu responsivel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var r = document.querySelector(":root");
 var check = document.querySelector("#checkbox");
 
+const menu = document.getElementById('menu');
+const links = document.getElementById('links');
+
 function myFunction_set() {
   if (checkbox.checked) {
     r.style.setProperty("--main-color", "#f02425");
@@ -11,4 +14,12 @@ function myFunction_set() {
     r.style.setProperty("--light-color", "#F0EDDA");
     r.style.setProperty("--text-color", "#202024");
   }
+}
+
+function Mostrar(){
+  if(links.style.display == "none"){
+    links.style.display = "block";
+    links.style.display = "flex";
+  }else if (links.style.display = "block")
+  links.style.display = "none";
 }
